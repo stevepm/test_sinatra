7be557e93c24315923de93788c9a9867c7a098b3
stevepm <steven.magelowitz@gmail.com>
2014-03-22 23:57:22 -0600

    completed tutorial

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source "https://rubygems.org"
 
 # gem "rails"
 gem 'sinatra', :git => 'git://github.com/sinatra/sinatra.git'
+gem 'shotgun', '~> 0.9'
--- basics.rb
+++ basics.rb
@@ -1,5 +1,41 @@
+require 'rubygems'
+
+require 'bundler/setup'
+
 require 'sinatra'
 
 get '/' do
   "hello world"
+end
+
+get '/about' do
+  'A little about me.'
+end
+
+get '/hello/:name' do
+  params[:name]
+end
+
+get '/form' do
+  erb :form
+end
+
+post '/form' do
+  "You said '#{params[:message]}'"
+end
+
+get '/secret' do
+  erb :secret
+end
+
+post '/secret' do
+  params[:secret].reverse
+end
+
+get '/decrypt/:secret' do
+  params[:secret].reverse
+end
+
+not_found do
+  halt 404, 'page not found'
 end
